61f921c1b6c1cd69d80c7671ee06630420bdfab0
Technology-Portfolio.Net <35184109+tecportnet@users.noreply.github.com>
2020-10-28 17:01:07 +0700

    Add files via upload

---
+++ radio.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+jwplayer('playersQmURABrCfuR').setup({
+file: 'https://www.microsoft.com/Windows/Downloads/Contents/Products/MediaPlayer/',
+file: 'https://masima.rastream.com/masima-pramborsjakarta?',
+image: 'https://3.bp.blogspot.com/-PyCXoPiHN78/WkufM8AkHNI/AAAAAAAAE0Y/hUWsgLu7JykDtUXYHlSw-xg1-d1HoKbrgCLcBGAs/s640/Radio%2BOnline.jpg',
+title: 'Prambors FM Radio Streaming By BangSiagian.id',
+width: '100%',
+height: '30',
+type: 'mp3',
+autoplay: 'true'
+});
+</script>
+<a href="https://www.bangsiagian.id/" rel="nofollow noopener">www.bangsiagian.id</a>
